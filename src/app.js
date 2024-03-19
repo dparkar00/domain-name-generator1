c19a95a1f06f10b9f91d2d59839aeec144606677
dparkar00 <67346510+dparkar00@users.noreply.github.com>
2024-03-19 00:28:18 +0000

    changes

--- src/app.js
+++ src/app.js
@@ -4,8 +4,30 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+// import { NoEmitOnErrorsPlugin } from "webpack";
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let fourthpart = [".com", ".net", ".org", ".net", ".us", ".io"];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let resArr = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < fourthpart.length; l++) {
+          let res = pronoun[i] + adj[j] + noun[k] + fourthpart[l];
+          resArr.push(res);
+          console.log(res);
+        }
+      }
+    }
+  }
+  for (let i = 0; i < resArr.length; i++) {
+    let newElement = document.createElement("h6");
+    newElement.innerHTML = resArr[i];
+    document.querySelector("#domainName").appendChild(newElement);
+  }
 };
